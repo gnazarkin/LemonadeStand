c60f3d575d30c341bcfcbf04225268868841abaf
gnazarkin <gleb.nazarkin@gmail.com>
2014-12-25 10:44:08 -0500

    Initial Commit

---
+++ LemonadeStandv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LemonadeStandv2
+//
+//  Created by Gleb Nazarkin on 2014-12-25.
+//  Copyright (c) 2014 Addo Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
